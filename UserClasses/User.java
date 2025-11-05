4d32f913b490a8adb4404009d9b1b463971b071e
sravyamalladi <52904993+sravyamalladi@users.noreply.github.com>
2025-11-05 14:03:49 -0500

    Made a user class

---
+++ UserClasses/User.java
@@ -0,0 +1,30 @@
+public class User {
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+   
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
