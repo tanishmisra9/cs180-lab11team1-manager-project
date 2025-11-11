c163b7ba36d25c5fe222a177641fa1d00f4a36cf
sebastianting <143572829+sebastianting@users.noreply.github.com>
2025-11-10 23:26:55 -0500

    Create ReservationDatabaseInterface for reservation management

---
+++ src/ReservationDatabaseInterface.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public interface ReservationDatabaseInterface {
+
+    boolean reserve(Auditorium a, Reservation r);
+
+    boolean cancelReservation(Reservation r);
+
+    List<Reservation> getReservations();
+
+    boolean saveDatabase();
+
+    boolean loadDatabase();
+
+    void addAuditorium(Auditorium a);
+
+    List<Auditorium> getAuditoriums();
+}
