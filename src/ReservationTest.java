e2f2f102f12737b396b3c1bb9cf419d27486129b
Vmous <vedjoshi2007@gmail.com>
2025-11-10 23:21:46 -0500

    Added final tests for BasicReservation

--- src/ReservationTest.java
+++ src/ReservationTest.java
@@ -1,5 +1,6 @@
 package src;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
@@ -78,7 +79,7 @@ public class ReservationTest {
         }
 
 
-        //Sample test from one of our earlier homeworks. 
+        //testing if illegal arguments are caught by constructor
         @Test(timeout = 1000)
         public void testConstructor() {
             String user = null;
@@ -120,6 +121,93 @@ public class ReservationTest {
                 System.out.println(e.getMessage());
             }
 
+            //negative row
+            user = "Taewoo Jung";
+            movie = "Jurassic Park";
+            date = LocalDateTime.now();
+            row = -1;
+            seat = 1;
+
+            try {
+                BasicReservation testReservation = new BasicReservation(user, movie, date, row, seat);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            //negative seat
+            user = "Taewoo Jung";
+            movie = "Jurassic Park";
+            date = LocalDateTime.now();
+            row = 1;
+            seat = -1;
+
+            try {
+                BasicReservation testReservation = new BasicReservation(user, movie, date, row, seat);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            //perfect parameters
+            //negative row
+            user = "Taewoo Jung";
+            movie = "Jurassic Park";
+            date = LocalDateTime.now();
+            row = 1;
+            seat = 1;
+
+            try {
+                BasicReservation testReservation = new BasicReservation(user, movie, date, row, seat);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
+
+        //testing if illegal arguments are caught by constructor
+        @Test(timeout = 1000)
+        public void testEqualsSameUser() {
+            String user = "Remy Jang";
+            String movie = "Jurassic Park";
+            LocalDateTime date = LocalDateTime.now();
+            int row = 3247892;
+            int seat = 32974932;
+
+            //same user name
+            try {
+                BasicReservation testReservationOne = new BasicReservation(user, movie, date, row, seat);
+                BasicReservation testReservationTwo = new BasicReservation(user, movie, date, row, seat);
+
+                boolean actual = testReservationOne.equals(testReservationTwo);
+                assertTrue("Usernames are the same but it claims the objects are different.", actual);
+
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        //testing if different
+        @Test(timeout = 1000)
+        public void testEqualsDiffUser() {
+            String user = "Remy Jang";
+            String movie = "Jurassic Park";
+            LocalDateTime date = LocalDateTime.now();
+            int row = 3247892;
+            int seat = 32974932;
+
+            try {
+                BasicReservation testReservationOne = new BasicReservation(user, movie, date, row, seat);
+                BasicReservation testReservationTwo = new BasicReservation("Taewoo Jung", movie, date, row, seat);
+
+                boolean actual = testReservationOne.equals(testReservationTwo);
+                assertTrue("Usernames are different with everything else the same, but it claims the objects are different.", actual);
+
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
     }
 }
